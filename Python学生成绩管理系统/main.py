7e4c1998dcac55137820603a1510caf5e4aa02f3
霜鱼丶 <frostfishc@163.com>
2022-04-10 11:29:38 +0800

    Python学生成绩管理系统

--- Python学生成绩管理系统/main.py
+++ Python学生成绩管理系统/main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from Login import *
 import tkinter as tk
 
-root = tk.Tk()
-root.title('欢迎进入学生成绩管理系统')
-LoginPage(root)
-root.mainloop()
+root = tk.Tk() #定义窗口
+root.title('欢迎进入学生成绩管理系统') #定义窗口标题
+LoginPage(root) #调用类（窗口所有功能在类里面实现） （这种方式类似继承）
+root.mainloop() #打开窗口
