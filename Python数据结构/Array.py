fc31a42a8c52eabf063160c72fcd6400237215c0
霜鱼丶 <frostfishc@163.com>
2022-04-22 12:48:25 +0800

    Python数据结构数组

---
+++ Python数据结构/Array.py
@@ -0,0 +1,15 @@
+class Array(object):
+    def __init__(self,capacity,fillValue=None):
+        self.items=list()
+        for count in range(capacity):
+            self.items.append(fillValue)
+    def __len__(self):
+        return len(self.items)
+    def __str__(self):
+        return str(self.items)
+    def __iter__(self):
+        return iter(self.items)
+    def __getitem__(self,index):
+        return self.items[index]
+    def __setitem__(self,index,value):
+        self.items[index]=value
